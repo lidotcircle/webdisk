20e84d4685e8fa29a2ed592da4e3fa834bf311b1
analyman <beam.analyman@gmail.com>
2021-02-04 10:29:49 +0000

    test

--- lib/__test__/config-db.test.ts
+++ lib/__test__/config-db.test.ts
@@ -1,9 +1,16 @@
 import { WDDatabase } from '../database/database';
 import path from 'path';
+import fs from 'fs';
 import { conf, GetConfig } from '../config';
 import { IDBUserSettings } from 'lib/database/modules/user_settings';
 import { CloseLogger } from '../logger';
 CloseLogger();
+
+try {
+    if(fs.statSync('/tmp/testwd.db')) {
+        fs.unlinkSync('/tmp/testwd.db');
+    }
+} catch {}
 
 
 test('config and database', async () => {
@@ -61,5 +68,46 @@ test('add/remove new user', async () => {
         await conf.DB.login('fakeuser', 'password');
         throw new Error('remove user fail');
     } catch {}
+});
+
+test('change password', async () => {
+    const token = await conf.DB.login('administrator', '123456');
+    await conf.DB.changePassword(token, '123456', '654321');
+    try {
+        await conf.DB.getUserInfo(token)
+        throw new Error('require remove login tokens');
+    } catch {}
+    const t2 = await conf.DB.login('administrator', '654321');
+    await conf.DB.changePassword(t2, '654321', '123456');
+});
+
+test('reset password', async () => {
+    const token = await conf.DB.login('administrator', '123456');
+    await conf.DB.generateInvitationCode(token, 1);
+    const invs = await conf.DB.getInvitationCodes(token);
+    expect(invs.length).toBeGreaterThan(1);
+    await conf.DB.addUser('fakeuser', 'password', invs[invs.length - 1][0]);
+    await conf.DB.resetPassword('fakeuser', 'oldpassword', invs[invs.length - 1][0]);
+    const t1 = await conf.DB.login('fakeuser', 'oldpassword');
+    expect(t1).not.toBeNull();
+
+    await conf.DB.removeUser(t1, 'fakeuser', 'oldpassword');
+});
+
+test('store pass', async () => {
+    const token = await conf.DB.login('administrator', '123456');
+    const passid = await conf.DB.newPass(token, 'https://example.com', 'user', 'password');
+    const allpass = await conf.DB.getAllPass(token);
+    expect(allpass.length).toBeGreaterThan(0);
+    expect(allpass[allpass.length - 1].passid).toBe(passid);
+    expect(allpass[allpass.length - 1].site).toBe('https://example.com');
+    expect(allpass[allpass.length - 1].account).toBe('user');
+    expect(allpass[allpass.length - 1].pass).toBe('password');
+    await conf.DB.changePass(token, passid, 'newpassword');
+    const allpass__ = await conf.DB.getAllPass(token);
+    expect(allpass__.length).toEqual(allpass.length);
+    expect(allpass__[allpass__.length - 1].pass).toBe('newpassword');
+    await conf.DB.deletePass(token, passid);
+    await conf.DB.logout(token);
 });
 
