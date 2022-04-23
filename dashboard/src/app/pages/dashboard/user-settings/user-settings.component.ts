119782a4af555104a4121695690580e4330e22cc
analyman <beam.analyman@gmail.com>
2022-04-23 17:59:18 +0000

    fix: settings swiper

--- dashboard/src/app/pages/dashboard/user-settings/user-settings.component.ts
+++ dashboard/src/app/pages/dashboard/user-settings/user-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
 import Swiper from 'swiper';
 import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, {EffectFade, EffectCube, EffectCoverflow, EffectFlip} from 'swiper/core';
@@ -10,16 +11,35 @@ SwiperCore.use([EffectFade, EffectCube, EffectCoverflow, EffectFlip]);
     templateUrl: './user-settings.component.html',
     styleUrls: ['./user-settings.component.scss']
 })
-export class UserSettingsComponent implements OnInit {
+export class UserSettingsComponent implements OnInit, OnDestroy, AfterViewInit {
     @ViewChild('swiper', {static: true})
     private settings: SwiperComponent;
 
     @ViewChild('settingsnav', {static: true})
     private nav: ElementRef;
 
-    constructor() { }
+    swiperDirection: string;
 
+    constructor() {
+        this.swiperDirection = window.innerWidth <= 600 ? 'horizontal' : 'vertical';
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(() => this.settings.swiperRef.update(), 0);
+    }
+
+    private resizeSubscription: Subscription;
     ngOnInit(): void {
+        const resizeHandler = () => {
+            this.swiperDirection = window.innerWidth <= 600 ? 'horizontal' : 'vertical';
+            // TODO update swiper after resize, otherwise incorrect direction
+        }
+        window.addEventListener("resize", resizeHandler);
+        this.resizeSubscription = new Subscription(() => window.removeEventListener("resize", resizeHandler));
+    }
+
+    ngOnDestroy(): void {
+        this.resizeSubscription.unsubscribe();
     }
 
     onSlideInit(swiper: Swiper) {
@@ -34,7 +54,7 @@ export class UserSettingsComponent implements OnInit {
         if(prev != null) {
             nav.children[prev].classList.remove('active');
         }
-        nav.children[curr].classList.add   ('active');
+        nav.children[curr].classList.add('active');
     }
 
     gotoPanel(event: MouseEvent) {
